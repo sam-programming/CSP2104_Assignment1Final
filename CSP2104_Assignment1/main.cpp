d1796806ba142cffee6857cd3541007cf6275cc7
Samuel Warner <72848532+sam-programming@users.noreply.github.com>
2021-04-05 14:36:42 +1000

    Update main.cpp

--- CSP2104_Assignment1/main.cpp
+++ CSP2104_Assignment1/main.cpp
@@ -2,19 +2,17 @@
 using namespace std;
 
 int main() {
-
-	Word wrd;
-	wrd.printDefinition();
 	//Use constructor to call loadDictionary on parameter filename
 	Dictionary_Part01 dict("dictionary2021.txt");
+	
 	const char EXIT = 'x';
 	cout << "Welcome to Dictionary Part 1!\n";
-
 	char user_comm = '0';
-	// Menu loops until user enters 'x'
+	//menu loops until user enters 'x'
 	while (user_comm != EXIT) {
 
 		string response;
+		//menu options
 		cout << "Enter '1' to Find a word.\n";
 		cout << "Enter '2' to List all word(s) that contain more than three 'z''s.\n";
 		cout << "Enter '3' to List all word(s) that contain a 'q' that is not followed by a 'u'.\n";
@@ -22,20 +20,20 @@ int main() {
 		cin >> response;
 		// To prevent cin buffer issues and unintentional input, take a string as cin and test it for length
 		// If the user puts a space in the input or it's too long, be mean to them
-		while (response.length() > 1) {
+		while (response.length() > 1) { //only allow input length of 1
 			cout << "Invalid input.  Please enter a single value corresponding to a menu item.\n";
 			cin >> response;
 		}		
-		user_comm = tolower(response[0]);
+		user_comm = tolower(response[0]); // take only the first char of response as input - user is warned
 		cin.ignore(10000, '\n'); // this will clear the cin buffer so that the next getline will work
 
 		switch (user_comm) {
 		case '1':
-		{ //Need braces when intialising a variable in a switch
+		{ //Need scope when intialising a variable in a switch
 			string userWrd = "";
 			cout << "Please enter a word...\n\n";
 			getline(cin, userWrd);
-			dict.binFindWord(userWrd);
+			dict.binFindWord(userWrd); // call binFindWord()
 			break;
 		}
 		case '2':
