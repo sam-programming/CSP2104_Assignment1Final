9d847a4e6904f852a5ee8827085ecdda0cf553a9
Samuel Warner <72848532+sam-programming@users.noreply.github.com>
2021-05-25 13:25:00 +0800

    Update main.cpp

--- CSP2104_Assignment1/main.cpp
+++ CSP2104_Assignment1/main.cpp
@@ -4,31 +4,33 @@ using namespace std;
 
 int main() {
 	//Use constructor to call loadDictionary on parameter filename
-	Dictionary_Part02 dict("dictionary2021.txt");
+	string filename = "dictionary2021.txt";
+	Dictionary_Part02 dict(filename);
 	vector<Word> noun_list;
 	Tri_Gram tri_gram("dictionary2021.txt");
 
 	const char EXIT = 'x';
-	cout << "Welcome to Dictionary Part 1!\n";
+	cout << "\n\n ---------------------------------------\n";
+	cout << " |    Welcome to Dictionary Part 2!    |\n";
+	cout << " ---------------------------------------\n";
 	char user_comm = '0';
 	//menu loops until user enters 'x'
 	while (user_comm != EXIT) {
-
 		string response;
 		//menu options
-		cout << "Enter '1' to Find a word.\n";
-		cout << "Enter '2' to List all word(s) that contain more than three 'z''s.\n";
-		cout << "Enter '3' to List all word(s) that contain a 'q' that is not followed by a 'u'.\n";
-		cout << "Enter '4' to List all word(s) that are both a Noun and a Verb.\n";
-		cout << "Enter '5' to List all word(s) that are palindromes.\n";
-		cout << "Enter '6' to Find all anagrams of a target word\n";
-		cout << "Enter '7' to play the Word Guessing Game!\n";
-		cout << "Enter '8' to play with Tri-Grams\n";
-		cout << "Enter " << EXIT << " to exit the program...\n";
+		cout << " - Enter '1' to Find a word.\n";
+		cout << " - Enter '2' to List all word(s) that contain more than three 'z''s.\n";
+		cout << " - Enter '3' to List all word(s) that contain a 'q' that is not followed by a 'u'.\n";
+		cout << " - Enter '4' to List all word(s) that are both a Noun and a Verb.\n";
+		cout << " - Enter '5' to List all word(s) that are palindromes.\n";
+		cout << " - Enter '6' to Find all anagrams of a target word\n";
+		cout << " - Enter '7' to play the Word Guessing Game!\n";
+		cout << " - Enter '8' to play with Tri-Grams\n";
+		cout << " - Enter " << EXIT << " to exit the program...\n";
 		cin >> response;
 		// To prevent cin buffer issues and unintentional input, take a string as cin and test it for length
 		// If the user puts a space in the input or it's too long, be mean to them
-		while (response.length() > 1) { //only allow input length of 1
+		while (response.length() > 1 ) { //only allow input length of 1
 			cout << "Invalid input.  Please enter a single value corresponding to a menu item.\n";
 			cin >> response;
 		}
@@ -43,7 +45,7 @@ int main() {
 			bool found;
 			cout << "Please enter a word...\n";
 			getline(cin, userWrd);
-			dict.binFindWord(userWrd); // call binFindWord()			
+			dict.findWord(userWrd); // call binFindWord()			
 			break;
 		}
 		case '2':
@@ -53,10 +55,14 @@ int main() {
 			dict.qButNoU();
 			break;
 		case '4':
+			cout << endl;
 			dict.listAllVerbNouns();
+			cout << endl;
 			break;
 		case '5':
+			cout << endl;
 			dict.listAllPalindromes();
+			cout << endl;
 			break;
 		case '6': 
 		{
@@ -75,9 +81,29 @@ int main() {
 			dict.guessingGame(noun_list);
 			break;
 		}
-		case '8': 
-		{
-			tri_gram.generate_Fake_Word(10, dict);
+		case '8': {
+			char menu_item = '0';
+			while (menu_item != '3') {
+				string usr_input = "";
+				cout << " - Enter '1' to Input two characters and get the three most likely characters to occur after those two characters. " << endl;
+				cout << " - Enter '2' to generate a list of 10 fake words, using Tri-Grams, that are not in the dictionary"<< endl;
+				cout << " - Enter '3' to return." << endl;
+				cin >> usr_input;
+				while (usr_input.length() > 1) {
+					cout << "Invalid input.  Please enter a single value corresponding to a menu item.\n";
+					cin >> usr_input;
+				}
+				menu_item = usr_input[0];
+				if (menu_item == '1') {
+					tri_gram.complete_Trigram(filename);
+					cout << endl;
+				}
+				else if (menu_item == '2') {
+					tri_gram.generate_Fake_Word(10, dict);
+					cout << endl;
+				}				
+			}
+			break;
 		}
 		case EXIT:
 			cout << "Exiting...\n";
